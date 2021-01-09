86c48959970c8ba1ce9b57dde65f41d6011f457b
Vladimir.Ivinski <64367581+vladimirivinski@users.noreply.github.com>
2021-01-09 04:59:57 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    else {
+        return Math.min(... array);
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    else {
+        return Math.max(... array);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    else {
+        return array.reduce((a, b) => a + b, 0) / array.length;
+    }
 }
